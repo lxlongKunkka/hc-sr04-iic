38d6eeb042450309e5b5e89e215e5dfb9a3f74f3
lxlongKunkka <110076790@qq.com>
2022-06-07 17:05:06 +0800

    更新 custom.ts, test.ts

--- custom.ts
+++ custom.ts
@@ -3,15 +3,15 @@ let HC_SR04_Address = 0X57;
 //% weight=20 color=#3333ff icon="\uf11b"
 namespace HC_SR04 {
 
-    //% blockID==Analog
+    //% blockID==HC_SR04
     //% block="Scan"
-    //% weight=90 advanced=true
-    export function ScanAllChannel(): number {
+    //% weight=90 
+    export function Scan(): number {
         let buf = pins.createBuffer(1);
         buf[0] = 0x01;
         pins.i2cWriteBuffer(HC_SR04_Address, buf);
         basic.pause(120)
-        let d = [-1, -1, -1];
+        let d = [0, 0, 0];
         for (let i = 0; i < 3; i++) {
             d[i] = pins.i2cReadNumber(HC_SR04_Address, NumberFormat.UInt8LE, false);
         }
