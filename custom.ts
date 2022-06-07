2915e49895e721ee6cc313b0c7ae3116159adabd
lxlongKunkka <110076790@qq.com>
2022-06-07 17:03:20 +0800

    更新 pxt.json, main.blocks, custom.ts

---
+++ custom.ts
@@ -0,0 +1,21 @@
+let HC_SR04_Address = 0X57;
+
+//% weight=20 color=#3333ff icon="\uf11b"
+namespace HC_SR04 {
+
+    //% blockID==Analog
+    //% block="Scan"
+    //% weight=90 advanced=true
+    export function ScanAllChannel(): number {
+        let buf = pins.createBuffer(1);
+        buf[0] = 0x01;
+        pins.i2cWriteBuffer(HC_SR04_Address, buf);
+        basic.pause(120)
+        let d = [-1, -1, -1];
+        for (let i = 0; i < 3; i++) {
+            d[i] = pins.i2cReadNumber(HC_SR04_Address, NumberFormat.UInt8LE, false);
+        }
+        let dis = (d[0]<<16 + d[1] << 8 + d[2]) / 10000;
+        return dis;
+    }
+}
